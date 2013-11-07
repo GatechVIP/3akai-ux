7ca5eec0f451f70745906047cba2ba0d85b992f1
Michael Darakananda <pongad@gmail.com>
2013-11-07 11:54:25 -0800

    Prettified the discussions

--- ui/js/discussion.js
+++ ui/js/discussion.js
@@ -94,12 +94,13 @@ require(['jquery','oae.core'], function($, oae) {
     var setUpTopic = function() {
         if (discussionProfile.description) {
             var topic = oae.api.util.security().encodeForHTMLWithLinks(discussionProfile.description).replace(/\n/g, '<br/>');
-            var vote ='<button id="button-vote">Vote: 100</button>';
+            var vote ='<button id="button-vote" class="btn btn-large" data-toggle="button" style="margin-right: 20px;"><i  class="icon-arrow-up"></i> 100</button>';
             $('#discussion-topic').html(vote + topic);
             var b = $('#button-vote');
             b.click(function() {
-                var s = (b.text() == 'Vote: 100') ? 'Vote: 101' : 'Vote: 100';
-                b.text(s);
+                var arrow = '<i class="icon-arrow-up"></i> ';
+                var s = (b.html() === arrow + '100') ? arrow + '101' : arrow + '100';
+                b.html(s);
             });
             $('#discussion-topic-container').show();
         }
